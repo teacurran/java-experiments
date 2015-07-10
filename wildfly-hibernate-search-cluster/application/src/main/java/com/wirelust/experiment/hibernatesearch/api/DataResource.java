bc7fbd3a1770d4fb3e47beba3cccfba1276329b1
Terrence Curran <tcurran@approachingpi.com>
2015-07-10 16:57:20 -0400

    search api endpoint

--- wildfly-hibernate-search-cluster/application/src/main/java/com/wirelust/experiment/hibernatesearch/api/DataResource.java
+++ wildfly-hibernate-search-cluster/application/src/main/java/com/wirelust/experiment/hibernatesearch/api/DataResource.java
@@ -5,20 +5,28 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.input.BOMInputStream;
+import org.apache.lucene.search.Query;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.FullTextQuery;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  * Date: 09-Jul-2015
@@ -61,5 +69,32 @@ public class DataResource {
 		}
 
 		return Response.status(Response.Status.NO_CONTENT).build();
+	}
+
+	@GET
+	@Path("/search")
+	public Response search(
+			@QueryParam("q")
+			final String queryString
+		) {
+
+		FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(em);
+
+		QueryBuilder qb = fullTextEntityManager.getSearchFactory()
+			.buildQueryBuilder()
+			.forEntity(City.class)
+			.get();
+
+		Query luceneQuery = qb.keyword()
+			.onFields("locode", "name", "state")
+			.matching(queryString).createQuery();
+
+		// wrap Lucene query in a javax.persistence.Query
+		FullTextQuery jpaQuery = fullTextEntityManager.createFullTextQuery(luceneQuery, City.class);
+
+		// execute search
+		List<City> result = (List<City>)jpaQuery.getResultList();
+
+		return Response.status(Response.Status.OK).entity(result).build();
 	}
 }
