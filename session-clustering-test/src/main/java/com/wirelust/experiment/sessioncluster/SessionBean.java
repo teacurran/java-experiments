5d228fa5e8667a40636468a4c6e95b9508d28402
Terrence Curran <tcurran@approachingpi.com>
2015-08-03 02:40:17 -0400

    session test code

--- session-clustering-test/src/main/java/com/wirelust/experiment/sessioncluster/SessionBean.java
+++ session-clustering-test/src/main/java/com/wirelust/experiment/sessioncluster/SessionBean.java
@@ -1,5 +1,10 @@
 package com.wirelust.experiment.sessioncluster;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 /**
@@ -8,5 +13,71 @@ import javax.inject.Named;
  * @author T. Curran
  */
 @Named
-public class SessionBean {
+@SessionScoped
+public class SessionBean implements Serializable {
+
+	HashMap<String, KeyValue> hashMap = new HashMap<>();
+
+	String key;
+	String value;
+
+	public void addValue() {
+		if (key != null) {
+			hashMap.put(key, new KeyValue(key, value));
+		}
+	}
+
+	public Collection<KeyValue> getList() {
+		return hashMap.values();
+	}
+
+	public HashMap<String, KeyValue> getHashMap() {
+		return hashMap;
+	}
+
+	public void setHashMap(HashMap<String, KeyValue> hashMap) {
+		this.hashMap = hashMap;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public class KeyValue {
+		String key;
+		String value;
+
+		KeyValue(String key, String value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public String getKey() {
+			return key;
+		}
+
+		public void setKey(String key) {
+			this.key = key;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
 }
